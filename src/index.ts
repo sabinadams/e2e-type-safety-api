6362eb00820338f4aca0d1b8226c89ba671ff779
Sabin Adams <sabintheworld@gmail.com>
2022-08-09 14:05:13 -0700

    First commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { createServer } from '@graphql-yoga/node'
+import { schema } from './schema'
+
+const port = Number(process.env.API_PORT) || 4000
+
+const server = createServer({
+    port,
+    schema
+})
+
+server.start().then(() => {
+    console.log(`🚀 Server started on port ${port}`)
+})
---
+++ src/schema.ts
@@ -0,0 +1,6 @@
+import { builder } from './builder'
+
+import "./models/User"
+import "./models/Message"
+
+export const schema = builder.toSchema({})
